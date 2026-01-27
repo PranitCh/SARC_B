31203beb0498f162942a744676f9a1c822e5b5af
Pranit Chitkara <pranit.chitkara@gmail.com>
2026-01-27 21:42:35 +0530

    Reapply "Added dark mode"
    This reverts commit 743dff46c254ef192f31e71a4a0bfb1e62e3ff51.

--- expenses/forms.py
+++ expenses/forms.py
@@ -5,20 +5,51 @@ class BudgetForm(forms.ModelForm):
     class Meta:
         model = Budget
         fields = ['name', 'amount']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Enter budget name',
+                'class': 'form-control',
+            }),
+            'amount': forms.NumberInput(attrs={
+                'placeholder': 'Amount',
+                'class': 'form-control',
+                'step': '0.01',
+            }),
+        }
 
 class GoalForm(forms.ModelForm):
     class Meta:
         model = Goal
         fields = ['name', 'target']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Enter goal name',
+                'class': 'form-control'
+            }),
+            'target': forms.NumberInput(attrs={
+                'placeholder': 'Target amount',
+                'class': 'form-control',
+                'step': '0.01'
+            }),
+        }
 
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ['desc', 'amount', 'is_income']
         widgets = {
-            'desc': forms.TextInput(attrs={'class': 'form-control'}),
-            'amount': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
-            'is_income': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'desc': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Description'
+                }),
+            'amount': forms.NumberInput(attrs=
+                {'class': 'form-control', 
+                'step': '0.01',
+                'placeholder': 'Enter amount' 
+                }),
+            'is_income': forms.CheckboxInput(attrs=
+                {'class': 'form-check-input',
+                }),
         }
     
     def save(self, commit=True, budget=None):
