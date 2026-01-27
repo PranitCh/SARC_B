91e28951beed1d1022b8836e7e6712ef65c9cb43
Pranit Chitkara <pranit.chitkara@gmail.com>
2026-01-27 21:40:07 +0530

    Added dark mode

--- expenses/urls.py
+++ expenses/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('', views.dashboard, name='dashboard'),
     path('budget/<int:pk>/', views.budget_detail, name='budget_detail'),
+    path('toggle-dark-mode/', views.toggle_dark_mode, name='toggle_dark_mode'),
+
 ]
