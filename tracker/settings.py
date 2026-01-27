785aa2f69d203b2005b1961a78f5a2a9602d185e
Pranit Chitkara <pranit.chitkara@gmail.com>
2026-01-27 22:29:50 +0530

    Deploy2

--- tracker/settings.py
+++ tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6lp(&bq2bwk3h8_aox8&)=h3hbl$v7n4dv*mi)342)d@z(tuhd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
