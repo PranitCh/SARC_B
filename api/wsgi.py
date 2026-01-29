805ec5ea7ce7ed17c965e7a6b58e747601ed0737
Pranit Chitkara <pranit.chitkara@gmail.com>
2026-01-29 15:33:17 +0530

    vercel

---
+++ api/wsgi.py
@@ -0,0 +1,8 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker.settings")
+application = get_wsgi_application()
+
+# Vercel expects a variable named "app"
+app = application
